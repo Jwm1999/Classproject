fc1232a57f946f7826e4e570fd2073aac52b5514
Jwm1999 <109264500+Jwm1999@users.noreply.github.com>
2022-07-13 23:40:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+//Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/EducationHistory', (req, res) => {
+    res.render('EducationHistory')
+})
+
+app.get('/Index', (req, res) => {
+    res.render('Index')
+})
+
+app.get('/WorkExperience', (req, res) => {
+    res.render('Workexperience')
+})
+
+app.get('/Schedule', (req, res) => {
+    res.render('Schedule')
+})
+
+app.get('/FeedBackForm', (req, res) => {
+    res.render('FeedbackForm')
+})
+
+// Listen on the port
+app.listen(port, () => console.info('Listening on port ' + port))
+  
